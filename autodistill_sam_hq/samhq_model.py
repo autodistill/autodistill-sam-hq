73317204ab3043980acaa455208568e687d7bd2f
James Gallagher <jamesg@jamesg.blog>
2023-12-06 10:17:07 +0000

    use load_image, change class id assignment logic

--- autodistill_sam_hq/samhq_model.py
+++ autodistill_sam_hq/samhq_model.py
@@ -1,16 +1,15 @@
 import os
 import urllib.request
-
-import numpy as np
-import torch
-import subprocess
-
-import supervision as sv
-
 from dataclasses import dataclass
+
 import cv2
+import numpy as np
+import supervision as sv
+import torch
+from segment_anything_hq import SamAutomaticMaskGenerator, sam_model_registry
+
 from autodistill.detection import CaptionOntology, DetectionBaseModel
-from segment_anything_hq import sam_model_registry, SamAutomaticMaskGenerator
+from autodistill.helpers import load_image
 
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -25,27 +24,31 @@ torch.use_deterministic_algorithms(False)
 HOME = os.path.expanduser("~")
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 @dataclass
 class SAMHQ(DetectionBaseModel):
     ontology: CaptionOntology
     box_threshold: float
     text_threshold: float
 
-    def __init__(self, ontology: CaptionOntology, box_threshold=0.35, text_threshold=0.25):
+    def __init__(
+        self, ontology: CaptionOntology, box_threshold=0.35, text_threshold=0.25
+    ):
         self.ontology = ontology
         self.predictor = load_SAMHQ()
         self.box_threshold = box_threshold
         self.text_threshold = text_threshold
 
     def predict(self, input: str) -> sv.Detections:
-        masks = self.predictor.generate(cv2.imread(input))
+        masks = self.predictor.generate(load_image(input, return_format="cv2"))
 
         results = sv.Detections.from_sam(masks)
 
-        results.class_id = np.array([0] * len(results.mask[0]))
+        results.class_id = [i for i in range(len(results.mask[0]))]
         results.confidence = np.array([1] * len(results.mask[0]))
 
         return results
+
 
 def load_SAMHQ():
     # Check if segment-anything library is already installed
@@ -65,8 +68,10 @@ def load_SAMHQ():
 
     SAM_ENCODER_VERSION = "vit_l"
 
-    sam = sam_model_registry[SAM_ENCODER_VERSION](checkpoint=SAM_CHECKPOINT_PATH).to(DEVICE)
-    
+    sam = sam_model_registry[SAM_ENCODER_VERSION](checkpoint=SAM_CHECKPOINT_PATH).to(
+        DEVICE
+    )
+
     sam_predictor = SamAutomaticMaskGenerator(sam)
 
     return sam_predictor
